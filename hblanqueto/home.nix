418ccc21e2a1b25d47bbc622fa45942d1a218b66
Mr. HBlanqueto <humbertoblanqueto@outlook.com>
2022-04-27 22:54:03 -0500

    Added more programs

--- hblanqueto/home.nix
+++ hblanqueto/home.nix
@@ -1,23 +1,43 @@
-{ config, pkgs, ... }:
+ config, pkgs, ... }:
 
 {
   home = {
     username = "Humberto Blanqueto";
     homeDirectory = "/home/hblanqueto";
 
+    sessionVariables = {
+      #NIXOS_OZONE_WL="1";
+    };
+
     packages = with pkgs; [
 
       #wpsoffice
+      #libreoffice-fresh-unwrapped
+      
+      tdesktop
+      #element-desktop
       obs-studio
-      neofetch
+      gnome.gnome-tweaks
+      #gnome-builder
 
+      neofetch
+      
+      #neovide
+      
       gnumake
       #gcc
       clang
       llvm
-      python3
-      lua
-      luarocks
+      
+      #rustc
+      #cargo
+      #python39
+      #lua5_4
+      #luarocks
+      
+      xdg-desktop-portal-wlr
+      xdg_utils
+      qt5.qtwayland
 
     ];
 
@@ -31,7 +51,20 @@
 
   programs = {
     home-manager.enable = true;
-
+    
+    google-chrome = {
+      enable = true;
+      package = pkgs.google-chrome;
+      commandLineArgs = [ 
+      "--enable-features=UseOzonePlatform"
+      "--ozone-platform=wayland --ignore-gpu-blocklist" 
+      "--enable-gpu-rasterization --enable-zero-copy" 
+      "--disable-gpu-driver-bug-workarounds"
+      "--enable-accelerated-video-decode"
+      "--enable-features=VaapiVideoDecoder"
+      "--use-gl=egl" ];
+    };
+    
     vscode = {
       enable = true;
       package = pkgs.vscode;
